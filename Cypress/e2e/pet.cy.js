404dc6c8989f0ff3dbb7cc233e0fb860ff2009e9
RMOUR <rodolfo_itg@yahoo.com.br>
2024-09-21 16:22:54 -0300

    Criamos o Put, DElete e Post com massa de teste

--- Cypress/e2e/pet.cy.js
+++ Cypress/e2e/pet.cy.js
@@ -21,7 +21,6 @@ describe('CRUD da entidade Pet', () => {
       expect(body.tags[0].name).to.eq(pet.tags[0].name)
       expect(body.status).to.eq(pet.status)
     })
-
   }) // termina POST
 
   // Get Pet
@@ -41,4 +40,55 @@ describe('CRUD da entidade Pet', () => {
     })
   }) // termina GET
 
-})
+  // Put Pet
+  it('PUT Pet', () => {
+    cy.request({
+      method: 'PUT',
+      url: '/pet',
+      body: petAlterado
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body.id).to.eq(petAlterado.id)
+      expect(body.name).to.eq(petAlterado.name)
+      expect(body.category.id).to.eq(petAlterado.category.id)
+      expect(body.category.name).to.eq(petAlterado.category.name)
+      expect(body.tags[0].id).to.eq(petAlterado.tags[0].id)
+      expect(body.tags[0].name).to.eq(petAlterado.tags[0].name)
+      expect(body.status).to.eq(petAlterado.status)
+    })
+  }) // termina PUT
+
+  // Delete Pet
+  it('DELETE Pet', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/pet/${pet.id}`
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200) // status code --> comunicação com a API
+      expect(body.code).to.eq(200) // process code --> processamento na API
+      expect(body.type).to.eq('unknown') // tipo desconhecido - padrão
+      expect(body.message).to.eq(`${pet.id}`) // código do animal que foi excluído
+      expect(body.message).to.eq((pet.id).toString()) // similar a linha anterior
+    })
+  }) // termina DELETE
+
+  // Data-driven POST
+  listaPets.forEach((element) => {
+    it(`POST Pet Data Driven - ${element.name}`, () => {
+      cy.request({
+        method: 'POST',
+        url: '/pet', // endpoint
+        body: element
+      }).then(({ status, body }) => {
+        expect(status).to.eq(200)
+        expect(body.id).to.eq(element.id)
+        expect(body.name).to.eq(element.name)
+        expect(body.category.id).to.eq(element.category.id)
+        expect(body.category.name).to.eq(element.category.name)
+        expect(body.tags[0].id).to.eq(element.tags[0].id)
+        expect(body.tags[0].name).to.eq(element.tags[0].name)
+        expect(body.status).to.eq(element.status)
+      })
+    }) // termina POST Data Driven
+  }) // termina o uso do listaPets
+}) // termina a describe
